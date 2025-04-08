5248442ff6c02f3108ff83ee7f244eeaeaabf5fe
smarquis20 <shaunmarquis@gmail.com>
2025-04-07 23:07:25 -0500

    api key update

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-const API_KEY = "";
+const API_KEY = "AIzaSyCHHXRafusOsrYooT8aBNbl_JXREMguLrU";
 const urlParams = new URLSearchParams(window.location.search);
 const addressesParam = urlParams.get("addresses");
 
