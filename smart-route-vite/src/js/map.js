6bf4ad9966b57da2298f99ff71368966e3c7d52e
smarquis20 <shaunmarquis@gmail.com>
2025-04-13 19:29:38 -0500

    update

--- smart-route-vite/src/js/map.js
+++ smart-route-vite/src/js/map.js
@@ -1,6 +1,6 @@
 import { saveNamedRoute } from "./storage";
 
-//const VITE_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const VITE_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 const urlParams = new URLSearchParams(window.location.search);
 const addressesParam = urlParams.get("addresses");
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
