30c176a294674305e90b9f2564f2a9f2150cf210
smarquis20 <shaunmarquis@gmail.com>
2025-04-13 21:09:23 -0500

    update test

--- smart-route-vite/src/js/map.js
+++ smart-route-vite/src/js/map.js
@@ -1,6 +1,6 @@
 import { saveNamedRoute } from "./routes.js";
 
-const VITE_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+//const VITE_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 console.log(VITE_API_KEY);
 const urlParams = new URLSearchParams(window.location.search);
 const addressesParam = urlParams.get("addresses");
@@ -74,7 +74,7 @@ window.initMap = initMap;
 
 function loadGoogleMapsScript() {
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${VITE_API_KEY}&callback=initMap`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCHHXRafusOsrYooT8aBNbl_JXREMguLrU&callback=initMap`;
     script.async = true;
     document.head.appendChild(script);
 }
